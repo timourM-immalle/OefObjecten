07ca1156ec782845c1b3045a2b0e3f80531c946d
timour.meeusen <timour.meeusen@B209-11>
2017-05-04 13:08:49 +0200

    Js: personen toegevoegd C#: klasse Persoon en List<Persoon> aangemaakt

--- Obectoef/js/index.js
+++ Obectoef/js/index.js
@@ -1,2 +1,18 @@
 ﻿// Your code here!
-console.log("klsksdkllkjfsjlkm");
+var personen = [];
+
+//1e manier
+var p1 = {};
+p1.naam = "Joske";
+p1.leeftijd = 13
+
+personen.push(p1);
+
+//2e manier
+var p2 = { naam: "Jantje", leeftijd: 15 };
+personen.push(p2);
+
+//3e manier
+personen.push({ naam: "mieke", leeftijd: 15 });
+
+personen.forEach(p => console.log(p.naam + " is " + p.leeftijd + " jaar oud.")); //p => ... = function(p) { return ... }
--- OefObjecten/Program.cs
+++ OefObjecten/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace OefObjecten
 {
+    public class Persoon
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+
+        public Persoon(string naam, int lt)
+        {
+            Naam = naam;
+            Leeftijd = lt;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            List<Persoon> personen = new List<Persoon>();
+            
         }
     }
 }

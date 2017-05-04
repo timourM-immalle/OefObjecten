7dcb99f72420bde05ccf5b41bd7cc7cc4a8969d8
timour.meeusen <timour.meeusen@B209-11>
2017-05-04 13:14:01 +0200

    C#: p1-3 toegevoegd

--- OefObjecten/Program.cs
+++ OefObjecten/Program.cs
@@ -11,6 +11,11 @@ namespace OefObjecten
         public string Naam { get; set; }
         public int Leeftijd { get; set; }
 
+        public Persoon()
+        {
+
+        }
+
         public Persoon(string naam, int lt)
         {
             Naam = naam;
@@ -23,7 +28,25 @@ namespace OefObjecten
         static void Main(string[] args)
         {
             List<Persoon> personen = new List<Persoon>();
-            
+
+            //1e manier
+            Persoon p1 = new Persoon { Naam = "Joske", Leeftijd = 13 };
+            personen.Add(p1);
+
+            //2e manier
+            Persoon p2 = new Persoon();
+            p2.Naam = "Jantje";
+            p2.Leeftijd = 14;
+            personen.Add(p2);
+
+            //3e manier
+            Persoon p3 = new Persoon("Mieke", 15);
+            personen.Add(p3);
+
+            foreach (Persoon p in personen)
+            {
+                Console.WriteLine(p.Naam + " is " + p.Leeftijd + " jaar oud.");
+            }
         }
     }
 }

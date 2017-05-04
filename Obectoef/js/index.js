1238eeec5fa61c0d1ee26df439e2570420f9c2fd
timour.meeusen <timour.meeusen@B209-11>
2017-05-04 13:54:29 +0200

    totaalprijs/Totaalprijs

--- Obectoef/js/index.js
+++ Obectoef/js/index.js
@@ -16,3 +16,15 @@ personen.push(p2);
 personen.push({ naam: "mieke", leeftijd: 15 });
 
 personen.forEach(p => console.log(p.naam + " is " + p.leeftijd + " jaar oud.")); //p => ... = function(p) { return ... }
+/////////////////////////////////////////
+var artikels = [];
+
+var artikel1 = { naam: "tafel", eenheidsprijs: 399.00, aantal: 1 };
+var artikel2 = { naam: "stoel", eenheidsprijs: 79.00, aantal: 4 };
+artikels.push(artikel1);
+artikels.push(artikel2);
+
+artikels.forEach(function (a) {
+    var totaalprijs = a.eenheidsprijs * a.aantal;
+    console.log(a.naam + "kost € " + a.eenheidsprijs + "/stuk. Men koop er " + a.aantal + " van aan en dus kost het in totaal € " + totaalprijs);
+});
